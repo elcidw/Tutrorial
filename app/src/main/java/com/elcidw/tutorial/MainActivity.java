7010685a3a77581b7507da536d7dfcb94b2bc07c
wsy <elcidw@163.com>
2017-09-01 22:52:04 -0500

    copy wx

--- app/src/main/java/com/elcidw/tutorial/MainActivity.java
+++ app/src/main/java/com/elcidw/tutorial/MainActivity.java
@@ -1,9 +1,7 @@
 package com.elcidw.tutorial;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
@@ -32,14 +29,6 @@ public class MainActivity extends AppCompatActivity
 
         queue = Volley.newRequestQueue(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -106,7 +95,7 @@ public class MainActivity extends AppCompatActivity
         }
         if (fragment != null) {
             FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.view_container, fragment);
+            fragmentTransaction.replace(R.id.main_container, fragment);
 
             fragmentTransaction.commit();
         }
